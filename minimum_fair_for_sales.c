0d3029dd857f0a85dae9e250738074d69f2fadc3
Amey <amey.jain7@gmail.com>
2019-05-26 10:46:14 +0530

    Minimum fair calculation corrected

--- minimum_fair_for_sales.c
+++ minimum_fair_for_sales.c
@@ -13,6 +13,8 @@ struct accessible {
   int acc_ctr;
 };
 
+static int pas = 0;
+
 struct accessible *create(int size) {
   int i;
   struct accessible *rtr = malloc(sizeof(struct accessible));
@@ -67,40 +69,48 @@ int get_nth_accessible(struct accessible *acc, int nth) {
 }
 
 //function to find minimum sum in matrix
-int find_min_sum(int **input_matrix, struct accessible *acc_rows, struct accessible *acc_cols) {
+int find_min_sum(int **input_matrix, struct accessible *acc_rows, struct accessible *acc_cols,int **output_matrix) {
   int i,j,sum = 0;
   int min_sum = -1;
-  static int pas = 0;
   //if matrix is equal to or greater than 2x2
   if((acc_rows->acc_ctr > 1) && (acc_cols->acc_ctr > 1)) {
 
     //select first row accessible and iterate on it
     i = get_nth_accessible(acc_rows, 1);
-
+    pas++;
     //Minimum sum to be compared for each value of j
     for(j = 0; j < length_of(acc_cols) ; j++) {
+  
       if(acc_cols->ptr[j] == 1) { //Coloumn is accessible
 	
 	//debug
-	printf("Working for fair %d\t i: %d j: %d\n", input_matrix[i][j],i,j);
+	//debug printf("Working for fair %d\t i: %d j: %d\tpas: %d\n", input_matrix[i][j],i,j,pas);
 
 	//remove access of i
 	remove_access(acc_rows, i);
 	remove_access(acc_cols, j);
-	sum = input_matrix[i][j] + find_min_sum(input_matrix, acc_rows, acc_cols);
+	sum = input_matrix[i][j] + find_min_sum(input_matrix, acc_rows, acc_cols,output_matrix);
 
-	if(min_sum == -1)
+	if(min_sum == -1) {
+	  output_matrix[pas][0] = i;
+	  output_matrix[pas][1] = j;
 	  min_sum = sum;
-	else 
-	  min_sum = min_sum < sum ? min_sum : sum;
-
-	printf("Minimum sum: %d sum: %d\n", min_sum,sum);
+	}
+	else {
+	  if(min_sum > sum) {
+	    output_matrix[pas][0] = i;
+	    output_matrix[pas][1] = j;
+	    min_sum = sum;
+	  }
+	}
+	//Debug	printf("Minimum sum: %d sum: %d pas:%d output_values:%d %d\n", min_sum,sum,pas,output_matrix[pas][0], output_matrix[pas][1]);
 	
 	//grant back access
 	grant_access(acc_rows, i);
 	grant_access(acc_cols, j);
       } 
     }
+    pas--;
     return min_sum;
   }
   
@@ -108,10 +118,11 @@ int find_min_sum(int **input_matrix, struct accessible *acc_rows, struct accessi
   else {
     i = get_nth_accessible(acc_rows, 1);
     j = get_nth_accessible(acc_cols, 1);
-    printf("Single element %d\n", input_matrix[i][j]);
+    //Debug    printf("Single element %d\n", input_matrix[i][j]);
     return input_matrix[i][j];
   }
 }
+
 
 void print_matrix(int **input_matrix, int n_src, int n_dst) {
   int i,j;
@@ -130,7 +141,7 @@ void print_matrix(int **input_matrix, int n_src, int n_dst) {
 
 int main() {
   int i,j;
-  int n_src, n_dst, **input_matrix, **output_matrix, *ptr, *ptr_,size;
+  int n_src, n_dst, **input_matrix, **output_matrix, *ptr, *ptr_,size,size_;
   struct accessible *acc_rows, *acc_cols;
   //input
   printf("Enter number of source: ");
@@ -142,7 +153,9 @@ int main() {
   //Sources are rows and destination columns
   size = (sizeof(int *) * n_src) + (sizeof(int) * n_src * n_dst);
   input_matrix = (int **) malloc(size);
-  output_matrix = (int **) malloc(size);
+
+  size_ = (sizeof(int *) * (n_src + 1)) + (sizeof(int) * (n_src + 1) * 2);
+  output_matrix = (int **) malloc(size_);
 
   //creating accessibility matrix
   acc_rows = create(n_src);
@@ -154,9 +167,8 @@ int main() {
   //pointing all rows to proper locations
   for(i = 0; i < n_src; i++){
     input_matrix[i] = ptr + (n_dst * i);
-    output_matrix[i] = ptr_ + (n_dst * i);
+    output_matrix[i] = ptr_ + (n_src * 2);
   }
-
   //INPUT LOOP
   //Loop for ith source
   for(i = 0; i < n_src; i++) {
@@ -166,8 +178,8 @@ int main() {
       printf("\tDestination %d: ", j + 1);
       scanf("%d", &input_matrix[i][j]);
 
+      output_matrix[i][0] = output_matrix[i][1] = 0;
       //initializing output_matrix[i][j]
-      output_matrix[i][j] = 0;
     }
   }
 
@@ -175,5 +187,7 @@ int main() {
   printf("Your input matrix is\n");
   print_matrix(input_matrix, n_src, n_dst);
 
-  printf("TEST: find_min_sum function return %d\n", find_min_sum(input_matrix, acc_rows, acc_cols));
+  printf("Minimum sum from sources to destination pair is %d\n", find_min_sum(input_matrix, acc_rows, acc_cols,output_matrix));
+  printf("Output\n");
+  print_matrix(output_matrix, n_src, 2);
 }
